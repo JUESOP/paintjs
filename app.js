12eae7a02d9586d340a72c6a93abe87b5e6d8ae4
JUESOP <sinangun@naver.com>
2022-02-10 23:32:25 +0900

    js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d"); 
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokStyle = "#2c2c2c"; 
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,9 +13,20 @@ function stopPainting() {
     painting = false;
 }
 
+function startPainting() {
+    painting = true;
+}
+
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+    if (!painting) {
+        ctx.beginPath(); // 선을 시작
+        ctx.moveTo(x,y);
+    } else {
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event) {
@@ -25,7 +43,7 @@ function onMouseLeave(event) {
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", onMouseLeave);
 }
---
+++ test.js
@@ -0,0 +1,27 @@
+const title = document.querySelector("div.hello:first-child h1");
+
+function hand() {
+    title.getElementsByClassName.color="blue";
+}
+
+function hand1() {
+    title.getElementsByClassName.color="blue";
+}
+
+function hand2() {
+    title.innerText = "ddd";
+}
+
+function hand3() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+title.addEventListener("click", hand);
+title.onclick = hand;
+title.addEventListener("mouseenter", hand1);
+title.addEventListener("mouseleave", hand2);
+
+window.addEventListener("resize", hand3);
+window.addEventListener("copy", hand3);
+offline
+online
